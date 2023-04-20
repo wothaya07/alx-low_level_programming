d7b99e67cdf8ed11f731401674d9235e60a4bfa5
Joan <joanwahome07@gmail.com>
2023-04-20 14:54:56 -0700

    using lu for bytes of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ long int li;
 long long int lli;
 char c;
 float f;
-printf("size of an char is :%lu.\n" ,(unsigned long)sizeof(c));
-printf("size of an int is :%lu.\n" ,(unsigned long)sizeof(i));
-printf("size of a long int is :%lu.\n" ,(unsigned long)sizeof(li));
-printf("size of a long long int is :%lu.\n" ,(unsigned long)sizeof(lli));
-printf("size of an float is :%lu.\n" ,(unsigned long)sizeof(f));
-return(0);
+printf("size of an char is :%lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("size of an int is :%lu bytes(s)\n", (unsigned long)sizeof(i));
+printf("size of a long int is :%lu bytes(s)\n", (unsigned long)sizeof(li));
+printf("size of a long long int is :%lu bytes(s)\n", sizeof(lli));
+printf("size of an float is :%lu bytes(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
