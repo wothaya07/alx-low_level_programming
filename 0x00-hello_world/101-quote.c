9431c1af2978136fed5869ade08e5153cd014e7a
Joan <joanwahome07@gmail.com>
2023-04-20 15:21:25 -0700

    standard error and fprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -the code prints the script to the standard error
+ * Returns 1 (Success)
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
