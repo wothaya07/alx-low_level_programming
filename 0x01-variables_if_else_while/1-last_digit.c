6b111531c457e5d40853d7a3db35c016b4af2b93
Joan <joanwahome07@gmail.com>
2023-04-24 12:49:17 -0700

    Using Mudulo and if statement

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more hheaders goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	int last_digit;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/*your code goes there */
+	last_digit = n % 10;
+	if (n > 5)
+	{printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}
+	else if (n == 0)
+	{printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+	else if (n > 6)
+	{printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	}
+		return (0);
+}
