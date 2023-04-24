0c7e7ab429bb626d88e3a6ef44f2a5cfe00d4d47
Joan <joanwahome07@gmail.com>
2023-04-24 13:05:13 -0700

    alphabets using for

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* program should print the alphabets*/
+int main(void)
+{
+char alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+{	putchar(alph);
+}
+	putchar('\n');
+	return (0);
+}
