0b3ce5bd6271e481abecc412df894fd1fa9d2f0d
Joan <joanwahome07@gmail.com>
2023-04-26 13:29:13 -0700

    alphabet in reverse ascii

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description:'print alphabets in reverse'
+ * Return value : Always 0
+ */
+int main(void)
+{
+int n = 122;
+while (n >= 97)
+{
+	putchar(n);
+	n--;
+}
+putchar('\n');
+return (0);
+}
